3bd3a77326af5ad4ebd59d5d7e0f967ee1f8a864
robotAlex <laguerto@ukr.net>
2014-12-19 01:34:59 +0300

    Comment seed file

--- seed.php
+++ seed.php
@@ -1,5 +1,6 @@
 <?php
 //Run composer install First!To populate db
+/*
 require 'vendor/autoload.php';
 $db = new PDO('mysql:host=127.0.0.1; dbname=drop_loc','root','');
 
@@ -8,3 +9,4 @@ $faker = Faker\Factory::create();
 foreach(range(1, 10000) as $x){
 	$db->query("INSERT INTO users (username) VALUES ('{$faker->userName}')");
 }
+*/
