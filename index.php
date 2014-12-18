d4f0743a220fac2a5cac39bfb2e9081755806173
robotAlex <laguerto@ukr.net>
2014-12-19 02:27:47 +0300

    +all working now

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Search</title>
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+	<form action="index.php" method="get">
+		<input type="text" name="user" id="users">
+		<input type="submit" value="Go">
+	</form>
+
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/typehead.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
